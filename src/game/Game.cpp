711f4caba05e5b009395f9ac54e9ab01c37425ce
Lauri Kenttä <lauri.kentta@gmail.com>
2011-02-07 23:14:19 +0200

    Add base class for the game logic.

---
+++ src/game/Game.cpp
@@ -0,0 +1,38 @@
+/*
+ * Game class implementation.
+ *
+ * Copyright 2011 Lauri Kenttä
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Game.hpp"
+
+const Scalar<SIUnit::Time> Game::stepTime = 1 / 30;
+
+void Game::runUntil(Scalar<SIUnit::Time> time) {
+	while (clock + stepTime < time) {
+		runStep();
+	}
+}
+
+void Game::runStep() {
+	// TODO: Update clock by some value and update the world respectively.
+}
+
+void Game::handleMessage(const Message& message) {
+	// TODO: Copy the message into an internal list, set message.timestamp = clock;
+}
---
+++ src/game/Game.hpp
@@ -0,0 +1,58 @@
+/*
+ * Game class definition.
+ *
+ * Copyright 2011 Lauri Kenttä
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _GAME_HPP
+#define _GAME_HPP
+
+#include "util/Scalar.hpp"
+#include "Message.hpp"
+
+/**
+ * The root of all game logic.
+ */
+class Game {
+	/** Game step duration. */
+	static const Scalar<SIUnit::Time> stepTime;
+
+	/** Keep track of game time. */
+	Scalar<SIUnit::Time> clock;
+
+	/**
+	 * Run the game one step forward.
+	 */
+	void runStep();
+public:
+	/**
+	 * Run the game up to the given time.
+	 *
+	 * @param time The time.
+	 */
+	void runUntil(Scalar<SIUnit::Time> time);
+
+	/**
+	 * Handle a message.
+	 *
+	 * @param message The message; timestamp will be overridden!
+	 */
+	void handleMessage(const Message& message);
+};
+
+#endif
