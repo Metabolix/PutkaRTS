bc3caa5002e1fbc6e7a3f3c436022dcd7bb3f317
Mika Katajamäki <masterlahha@gmail.com>
2011-03-17 21:25:42 +0200

    Add GUI::Widget::List class.

---
+++ src/gui/widget/List.cpp
@@ -0,0 +1,255 @@
+/*
+ * List widget class implementation.
+ *
+ * Copyright 2011 Jaakko Puntila
+ * Copyright 2011 Mika Katajamäki
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "List.hpp"
+
+#include <algorithm>
+#include <boost\bind.hpp>
+
+GUI::Widget::List::Item GUI::Widget::List::invalidItem = {"", ""};
+
+GUI::Widget::List::List(float x, float y, float width, float height, CallbackType callback):
+	Widget(x, y, width, height),
+	action(callback),
+	lineHeight(20),
+	borderWidth(4),
+	scrollbar(x + width - 16 - 4, y + 4, height - 2 * 4, 16, true) {
+	selected = items.end();
+	scrollPosition = 0;
+}
+
+bool GUI::Widget::List::handleEvent(const sf::Event& e, const sf::RenderWindow& window) {
+	const int lines = (position.GetHeight() - 2 * borderWidth) / lineHeight;
+
+	if ((int)items.size() > lines) {
+		if (scrollbar.handleEvent(e, window)) {
+			setScrollPosition(((items.size() - lines) * scrollbar.getScrollPosition()) + 0.5f);
+			return true;
+		}
+	}
+
+	if (e.Type == sf::Event::MouseButtonPressed && e.MouseButton.Button == sf::Mouse::Left) {
+		sf::Vector2f mouse(window.ConvertCoords(e.MouseButton.X, e.MouseButton.Y));
+
+		sf::FloatRect itemArea = position;
+		itemArea.Right -= borderWidth;
+		itemArea.Left += borderWidth;
+		itemArea.Top += borderWidth;
+		itemArea.Bottom -= borderWidth;
+
+		if (itemArea.Contains(mouse.x, mouse.y)) {
+			int itemIndex = scrollPosition + (mouse.y - itemArea.Top) / lineHeight;
+
+			selected = items.begin();
+			for (int i = 0; i < itemIndex && selected != items.end(); i++) {
+				selected++;
+			}
+
+			if (action) {
+				action(*selected);
+			}
+
+			return true;
+		}
+	} else if (e.Type == sf::Event::MouseWheelMoved) {
+		const sf::Input& input = window.GetInput();
+		sf::Vector2f mouse(window.ConvertCoords(input.GetMouseX(), input.GetMouseY()));
+
+		sf::FloatRect itemArea = position;
+		itemArea.Right -= borderWidth;
+		itemArea.Left += borderWidth;
+		itemArea.Top += borderWidth;
+		itemArea.Bottom -= borderWidth;
+
+		if (itemArea.Contains(mouse.x, mouse.y)) {
+			scroll(-e.MouseWheel.Delta);
+			scrollbar.setScrollPosition((float)scrollPosition / (items.size() - lines));
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void GUI::Widget::List::draw(sf::RenderWindow& window) {
+	int lines = (position.GetHeight() - 2 * borderWidth) / lineHeight;
+
+	sf::String tmpText("|");
+	float scale = lineHeight / tmpText.GetRect().GetHeight() * 0.95f;
+	tmpText.Scale(scale, scale);
+
+	window.Draw(
+		sf::Shape::Rectangle(
+			position.Left + borderWidth,
+			position.Top + borderWidth,
+			position.Right - borderWidth,
+			position.Bottom - borderWidth,
+			sf::Color(0x55, 0x44, 0x33),
+			borderWidth,
+			sf::Color(0, 0, 0)
+		)
+	);
+	
+	int counter = 0;
+
+	const sf::Input& input = window.GetInput();
+	sf::Vector2f mouse(window.ConvertCoords(input.GetMouseX(), input.GetMouseY()));
+
+	int hoverIndex = -1;
+
+	sf::FloatRect itemArea = position;
+	itemArea.Right -= borderWidth + 16;
+	itemArea.Left += borderWidth;
+	itemArea.Top += borderWidth;
+	itemArea.Bottom -= borderWidth;
+
+	if (itemArea.Contains(mouse.x, mouse.y)) {
+		hoverIndex = scrollPosition + (mouse.y - itemArea.Top) / lineHeight;
+	}
+
+	for (ItemContainerType::const_iterator i = items.begin(); i != items.end() && counter < lines + scrollPosition; ++i, ++counter) {
+		if (counter < scrollPosition) {
+			continue;
+		}
+
+		tmpText.SetText(i->text);
+		
+		if (selected == i) {
+			tmpText.SetColor(sf::Color(0xFF, 0xFF, 0xFF));
+			window.Draw(
+				sf::Shape::Rectangle(
+					position.Left + borderWidth,
+					position.Top + borderWidth + (counter - scrollPosition) * lineHeight,
+					position.Right - borderWidth,
+					position.Top + borderWidth + (counter + 1 - scrollPosition) * lineHeight,
+					sf::Color(0, 162, 232)
+				)
+			);
+		} else if (counter == hoverIndex) {
+			tmpText.SetColor(sf::Color(0, 0, 0));
+			window.Draw(
+				sf::Shape::Rectangle(
+					position.Left + borderWidth,
+					position.Top + borderWidth + (counter - scrollPosition) * lineHeight,
+					position.Right - borderWidth,
+					position.Top + borderWidth + (counter + 1 - scrollPosition) * lineHeight,
+					sf::Color(0x75, 0x64, 0x53)
+				)
+			);
+		} else {
+			tmpText.SetColor(sf::Color(0, 0, 0));
+		}
+
+		tmpText.SetPosition(position.Left + 2 * borderWidth, position.Top + borderWidth + (counter - scrollPosition) * lineHeight);
+		window.Draw(tmpText);
+	}
+
+	if ((int)items.size() > lines) {
+		scrollbar.draw(window);
+	}
+}
+
+void GUI::Widget::List::setScrollPosition(int pos) {
+	int lines = (position.GetHeight() - 2 * borderWidth) / lineHeight;
+
+	pos = std::max(0, std::min(pos, (int)(items.size()-lines)));
+	scrollPosition = pos;
+}
+
+void GUI::Widget::List::scroll(int n) {
+	setScrollPosition(scrollPosition+n);
+}
+
+const GUI::Widget::List::KeyType& GUI::Widget::List::getSelectedKey() const {
+	if (selected != items.end()) {
+		return selected->key;
+	}
+
+	return invalidItem.key;
+}
+
+const std::string& GUI::Widget::List::getSelectedText() const {
+	if (selected != items.end()) {
+		return selected->text;
+	}
+
+	return invalidItem.text;
+}
+
+void GUI::Widget::List::insertItem(const GUI::Widget::List::KeyType& key, const std::string& text) {
+	Item tmp;
+	tmp.key = key;
+	tmp.text = text;
+
+	for (ItemContainerType::iterator i = items.begin(); i != items.end(); ++i) {
+		if ( i->key == key ) {
+			i->text = text;
+			return;
+		}
+	}
+
+	items.push_back(tmp);
+}
+
+void GUI::Widget::List::eraseItem(const GUI::Widget::List::KeyType& key) {
+	for (ItemContainerType::iterator i = items.begin(); i != items.end(); ++i) {
+		if (i->key == key) {
+			if (selected == i) {
+				selected == items.end();
+			}
+
+			items.erase(i);
+			return;
+		}
+	}
+}
+
+float GUI::Widget::List::getBorderWidth() const {
+	return borderWidth;
+}
+
+float GUI::Widget::List::getLineHeight() const {
+	return lineHeight;
+}
+
+void GUI::Widget::List::setLineHeight(float lineHeight_) {
+	lineHeight = std::max(1.0f, std::min(lineHeight_, position.GetHeight() - 2 * borderWidth));
+}
+
+unsigned GUI::Widget::List::countItems() const {
+	return items.size();
+}
+
+void GUI::Widget::List::setPosition(float x, float y) {
+	position.Offset(x - position.Left, y - position.Top);
+
+	scrollbar.setPosition(position.Left + position.GetWidth() - 16 - getBorderWidth(), position.Top + getBorderWidth());
+	scrollbar.setSize(16, position.GetHeight() - 2 * getBorderWidth());
+}
+
+void GUI::Widget::List::setSize(float width, float height) {
+	position.Right = position.Left + width;
+	position.Bottom = position.Top + height;
+
+	scrollbar.setPosition(position.Left + width - 16 - getBorderWidth(), position.Top + getBorderWidth());
+	scrollbar.setSize(height - 2 * getBorderWidth(), 16);
+}
---
+++ src/gui/widget/List.hpp
@@ -0,0 +1,200 @@
+/*
+ * List widget class definition.
+ *
+ * Copyright 2011 Jaakko Puntila
+ * Copyright 2011 Mika Katajamäki
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PUTKARTS_GUI_Widget_List_HPP
+#define PUTKARTS_GUI_Widget_List_HPP
+
+#include "Widget.hpp"
+#include "Slider.hpp"
+
+#include <list>
+#include <boost/function.hpp>
+
+namespace GUI {
+	namespace Widget {
+		class List;
+	}
+}
+
+/**
+ * Class for the list widget.
+ */
+class GUI::Widget::List: public Widget {
+public:
+	/** Typedef for Item::key*/
+	typedef std::string KeyType;
+
+	/** Struct for the items on the list. */
+	struct Item {
+		KeyType key;
+		std::string text;
+	};
+
+	/** Typedef for item container. */
+	typedef std::list<Item> ItemContainerType;
+
+	/** Callback function type. */
+	typedef boost::function<void(const Item&)> CallbackType;
+
+	/** Invalid item. Used when there is no valid one to use. */
+	static Item invalidItem;
+
+private:
+	/** Container for the items. */
+	ItemContainerType items;
+
+	/** Iterator that points to the selected item or to the end of the container when no item is selected. */
+	ItemContainerType::const_iterator selected;
+
+	/** Slider to serve as scrollbar. */
+	GUI::Widget::Slider scrollbar;
+
+	/**  Callback function for the list. */
+	CallbackType action;
+
+	/** Current scroll position. Lines from the top of the list. */
+	int scrollPosition;
+
+protected:
+	/** Rendering height of one line. */
+	float lineHeight;
+
+	/** Width of list border */
+	float borderWidth;
+
+public:
+	/**
+	 * Create a list.
+	 *
+	 * @param x X coordinate of the list.
+	 * @param y Y coordinate of the list.
+	 * @param width Width of the list.
+	 * @param height Height of the list.
+	 * @param callback The action to take when clicked.
+	 */
+	List(float x, float y, float width, float height, CallbackType callback = NULL);
+
+	/**
+	 * Handle events, namely mouse clicks.
+	 *
+	 * @param e The event.
+	 * @param window The window of the event.
+	 * @return True, if this list handled the event.
+	 */
+	virtual bool handleEvent(const sf::Event& e, const sf::RenderWindow& window);
+
+	/**
+	 * Draw the list.
+	 *
+	 * @param window The render window.
+	 */
+	virtual void draw(sf::RenderWindow& window);
+
+	/**
+	 * Set the scroll position.
+	 *
+	 * @param pos The new scroll position. (Number of first line to be drawn)
+	 */
+	virtual void setScrollPosition(int pos);
+
+	/**
+	 * Scroll n lines.
+	 *
+	 * @param n Number of lines to scroll.
+	 */
+	virtual void scroll(int n);
+
+	/**
+	 * Get the key of the currently selected item key.
+	 *
+	 * @return Returns the currently selected item. If no item is selected returns dummy item GUI::Widget::List::invalidItem.
+	 */
+	virtual const KeyType& getSelectedKey() const;
+
+	/**
+	 * Get the key of the currently selected item text.
+	 *
+	 * @return Returns the currently selected item. If no item is selected returns dummy item GUI::Widget::List::invalidItem.
+	 */
+	virtual const std::string& getSelectedText() const;
+
+	/**
+	 * Insert new item into the list or replace existing one.
+	 *
+	 * @param key Key for the item.
+	 * @param text Text for the item.
+	 */
+	virtual void insertItem(const KeyType& key, const std::string& text);
+
+	/**
+	 * Erase item with matching key from the list.
+	 *
+	 * @param key Key for the item to erase.
+	 */
+	virtual void eraseItem(const KeyType& key);
+
+	/**
+	 * Get the borderWidth
+	 *
+	 * @return Returns the border width.
+	 */
+	virtual float getBorderWidth() const;
+
+	/**
+	 * Get the line height
+	 *
+	 * @return Returns the line height.
+	 */
+	virtual float getLineHeight() const;
+
+	/**
+	 * Set the line height
+	 *
+	 * @param lineHeight New line height.
+	 */
+	virtual void setLineHeight(float lineHeight);
+
+	/**
+	 * Get the number of items in the list.
+	 *
+	 * @return Returns the number of items in the list.
+	 */
+	virtual unsigned countItems() const;
+
+	/**
+	 * Set new position the widget.
+	 *
+	 * @param x New x position
+	 * @param y New y position
+	 */
+	virtual void setPosition(float x, float y);
+
+	/**
+	 * Set new size for the widget.
+	 *
+	 * @param width New width
+	 * @param height New height
+	 */
+	virtual void setSize(float width, float height);
+};
+
+#endif
