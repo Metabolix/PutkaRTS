916feccac22c5d0d0535dcea3a112c401e7dd9b9
Mika Katajamäki <masterlahha@gmail.com>
2011-03-17 21:38:02 +0200

    Add GUI::Widget::DropDown class.

---
+++ src/gui/widget/DropDown.cpp
@@ -0,0 +1,123 @@
+/*
+ * DropDown widget class implementation.
+ *
+ * Copyright 2011 Jaakko Puntila
+ * Copyright 2011 Mika Katajamäki
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "DropDown.hpp"
+
+#include <algorithm>
+#include <boost\bind.hpp>
+
+
+GUI::Widget::DropDown::DropDown(float x, float y, float width, float height, float dropDownMaxHeight_, CallbackType callback):
+	Widget(x, y, width, height),
+	dropButton("V", x + width - height + 2, y + 2, height - 4, height - 4, boost::bind(&GUI::Widget::DropDown::dropButtonPressed, this)),
+	dropList(x, y + height, width, dropDownMaxHeight_, boost::bind(&GUI::Widget::DropDown::itemSelected, this, _1)),
+	open(false) {
+	dropList.setLineHeight(height - 2 * dropList.getBorderWidth());
+	dropDownMaxHeight = dropDownMaxHeight_;
+	calculateDropDownSize();
+}
+
+void GUI::Widget::DropDown::dropButtonPressed() {
+	open = true;
+}
+
+void GUI::Widget::DropDown::itemSelected(const List::Item& item) {
+	open = false;
+
+	if(action) {
+		action(item);
+	}
+}
+
+void GUI::Widget::DropDown::calculateDropDownSize() {
+	float height = std::max(1, std::min<int>(dropList.countItems(), dropDownMaxHeight / dropList.getLineHeight()));
+	height *= dropList.getLineHeight();
+	height +=  + 2 * dropList.getBorderWidth();
+	
+	dropList.setSize(position.GetWidth(), height);
+}
+
+bool GUI::Widget::DropDown::handleEvent(const sf::Event& e, const sf::RenderWindow& window) {
+	
+	if (dropButton.handleEvent(e, window)) {
+		return true;
+	}
+
+	if (open) {
+		if (dropList.handleEvent(e, window)) {
+			return true;
+		} else if (e.Type == sf::Event::MouseButtonPressed && e.MouseButton.Button == sf::Mouse::Left) {
+			open = false;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void GUI::Widget::DropDown::draw(sf::RenderWindow& window) {
+	float bw = dropList.getBorderWidth();
+
+	window.Draw(
+		sf::Shape::Rectangle(
+			Widget::position.Left + bw,
+			Widget::position.Top + bw,
+			Widget::position.Right - bw,
+			Widget::position.Bottom - bw,
+			sf::Color(0x55, 0x44, 0x33),
+			bw,
+			sf::Color(0, 0, 0)
+		)
+	);
+
+	sf::String tmpText("|");
+	float scale = (Widget::position.GetHeight() - 2 * bw) / tmpText.GetRect().GetHeight() * 0.95f;
+	tmpText.Scale(scale, scale);
+	tmpText.SetColor(sf::Color(0, 0, 0));
+
+	tmpText.SetText(dropList.getSelectedText());
+	tmpText.SetPosition(Widget::position.Left + 2 * bw, Widget::position.Top + bw);
+	window.Draw(tmpText);
+
+	dropButton.draw(window);
+
+	if (open) {
+		dropList.draw(window);
+	}
+}
+
+const GUI::Widget::List::KeyType& GUI::Widget::DropDown::getSelectedKey() const {
+	return dropList.getSelectedKey();
+}
+
+const std::string& GUI::Widget::DropDown::getSelectedText() const {
+	return dropList.getSelectedText();
+}
+
+void GUI::Widget::DropDown::insertItem(const GUI::Widget::List::KeyType& key, const std::string& text) {
+	dropList.insertItem(key, text);
+	calculateDropDownSize();
+}
+
+void GUI::Widget::DropDown::eraseItem(const GUI::Widget::List::KeyType& key) {
+	dropList.eraseItem(key);
+}
---
+++ src/gui/widget/DropDown.hpp
@@ -0,0 +1,131 @@
+/*
+ * DropDown widget class definition.
+ *
+ * Copyright 2011 Jaakko Puntila
+ * Copyright 2011 Mika Katajamäki
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PUTKARTS_GUI_Widget_DropDown_HPP
+#define PUTKARTS_GUI_Widget_DropDown_HPP
+
+#include "Widget.hpp"
+#include "Button.hpp"
+#include "List.hpp"
+
+#include <boost/function.hpp>
+
+namespace GUI {
+	namespace Widget {
+		class DropDown;
+	}
+}
+
+/**
+ * Class for the drop-down menu widget.
+ */
+class GUI::Widget::DropDown: public Widget {
+public:
+	/** Button that opens the drop-down menu */
+	GUI::Widget::Button dropButton;
+	GUI::Widget::List dropList;
+
+	/** Is the drop-down menu open? */
+	bool open;
+
+	/** Callback function type. */
+	typedef boost::function<void(const List::Item&)> CallbackType;
+
+private:
+	/** Callback function */
+	CallbackType action;
+
+protected:
+	/**
+	* Opens the menu.
+	*/
+	virtual void dropButtonPressed();
+
+	/**
+	* Closes the menu and calls action().
+	*/
+	virtual void itemSelected(const List::Item& item);
+
+	/** Max height for the drop-down part*/
+	float dropDownMaxHeight;
+
+	virtual void calculateDropDownSize();
+
+public:
+	/**
+	 * Create a drop-down menu.
+	 *
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 * @param width Width.
+	 * @param height Height.
+	 * @param callback The action to take when item is selected.
+	 */
+	DropDown(float x, float y, float width, float height, float dropDownMaxHeight_, CallbackType callback = NULL);
+
+	/**
+	 * Handle events, namely mouse clicks.
+	 *
+	 * @param e The event.
+	 * @param window The window of the event.
+	 * @return True, if this drop-down menu handled the event.
+	 */
+	virtual bool handleEvent(const sf::Event& e, const sf::RenderWindow& window);
+
+	/**
+	 * Draw the drop-down menu.
+	 *
+	 * @param window The render window.
+	 */
+	virtual void draw(sf::RenderWindow& window);
+
+	/**
+	 * Get the key of the currently selected item key.
+	 *
+	 * @return Returns the currently selected item. If no item is selected returns dummy item GUI::Widget::List::invalidItem.
+	 */
+	virtual const GUI::Widget::List::KeyType& getSelectedKey() const;
+
+	/**
+	 * Get the key of the currently selected item text.
+	 *
+	 * @return Returns the currently selected item. If no item is selected returns dummy item GUI::Widget::List::invalidItem.
+	 */
+	virtual const std::string& getSelectedText() const;
+
+	/**
+	 * Insert new item into the menu or replace existing one.
+	 *
+	 * @param key Key for the item.
+	 * @param text Text for the item.
+	 */
+	virtual void insertItem(const GUI::Widget::List::KeyType& key, const std::string& text);
+
+	/**
+	 * Erase item with matching key from the menu.
+	 *
+	 * @param key Key for the item to erase.
+	 */
+	virtual void eraseItem(const GUI::Widget::List::KeyType& key);
+};
+
+#endif
