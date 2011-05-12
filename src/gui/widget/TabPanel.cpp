013566aee40802402f12191f585f282d72fb7473
Mika Katajamäki <masterlahha@gmail.com>
2011-05-13 02:27:10 +0300

    Add TabPanel widget.

---
+++ src/gui/widget/TabPanel.cpp
@@ -0,0 +1,100 @@
+/*
+ * Tab panel widget class implementation.
+ *
+ * Copyright 2011 Mika Katajamäki
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "TabPanel.hpp"
+
+#include <boost/bind.hpp>
+
+GUI::Widget::TabPanel::TabPanel(float x, float y, float width, float height):
+	TabManager(x, y, width, height) {
+}
+
+bool GUI::Widget::TabPanel::handleEvent(const sf::Event& e, const sf::RenderWindow& window) {
+	for (ButtonListType::iterator i = buttons.begin(); i != buttons.end(); ++i) {
+		if (i->button->handleEvent(e, window)) {
+			return true;
+		}
+	}
+
+	return TabManager::handleEvent(e, window);
+}
+
+void GUI::Widget::TabPanel::draw(sf::RenderWindow& window) {
+	TabManager::draw(window);
+
+	for (ButtonListType::const_iterator i = buttons.begin(); i != buttons.end(); ++i) {
+		i->button->draw(window, (i->key == selectedTabKey));
+	}
+}
+
+void GUI::Widget::TabPanel::addButton(const std::string& label, const GUI::Widget::TabManager::TabKeyType& key) {
+	Button tmp;
+	tmp.button.reset(new GUI::Widget::Button(label, 0.0f, 0.0f, 1.0f, 1.0f, boost::bind(&GUI::Widget::TabManager::selectTab, this, key)));
+	tmp.key = key;
+
+	buttons.push_back(tmp);
+	resizeButtons();
+}
+
+void GUI::Widget::TabPanel::removeButton(const GUI::Widget::TabManager::TabKeyType& key) {
+	for (ButtonListType::iterator i = buttons.begin(); i != buttons.end(); ++i) {
+		if (i->key == key) {
+			i = buttons.erase(i);
+		}
+	}
+	resizeButtons();
+}
+
+void GUI::Widget::TabPanel::resizeButtons() {
+	//TODO: Panel scrolling if need for it arises.
+
+	if (buttons.empty()) {
+		return;
+	}
+
+	float buttonWidth = position.GetWidth() / buttons.size();
+	int count = 0;
+
+	for (ButtonListType::iterator i = buttons.begin(); i != buttons.end(); ++i) {
+		i->button->setPosition(position.Left + count * buttonWidth, position.Top, buttonWidth, position.GetHeight()); 
+		count++;
+	}
+}
+
+void GUI::Widget::TabPanel::updateState(sf::RenderWindow& window) {
+	for (ButtonListType::iterator i = buttons.begin(); i != buttons.end(); ++i) {
+		i->button->updateState(window);
+	}
+
+	TabManager::updateState(window);
+}
+
+GUI::Widget::TabPanel::TabContainerPtrType GUI::Widget::TabPanel::addTab(const TabKeyType& key, const std::string& title, TabContainerPtrType container) {
+	TabContainerPtrType ret = TabManager::addTab(key, container);
+	addButton(title, key);
+	
+	return ret;
+}
+
+void GUI::Widget::TabPanel::removeTab(const TabKeyType& key) {
+	TabManager::removeTab(key);
+	removeButton(key);
+}
---
+++ src/gui/widget/TabPanel.hpp
@@ -0,0 +1,129 @@
+/*
+ * Tab panel widget class definition.
+ *
+ * Copyright 2011 Mika Katajamäki
+ *
+ * This file is part of PutkaRTS.
+ *
+ * PutkaRTS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PutkaRTS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PUTKARTS_GUI_Widget_TabPanel_HPP
+#define PUTKARTS_GUI_Widget_TabPanel_HPP
+
+#include "gui/widget/TabManager.hpp"
+#include "gui/widget/Button.hpp"
+
+#include <boost/shared_ptr.hpp>
+#include <string>
+
+namespace GUI {
+	namespace Widget {
+		class TabPanel;
+	}
+}
+
+/**
+ * Class for the tab panel widget.
+ */
+class GUI::Widget::TabPanel: public GUI::Widget::TabManager {
+public:
+	/** Struct for buttons. */
+	struct Button {
+		GUI::Widget::TabManager::TabKeyType key;
+		boost::shared_ptr<GUI::Widget::Button> button;
+	};
+
+	/** Typedef for button lists. */
+	typedef std::list<Button> ButtonListType;
+
+private:
+	/** List for buttons. */
+	ButtonListType buttons;
+
+	/**
+	 * Add new button into the list.
+	 *
+	 * @param label Text rendered on the button.
+	 * @param key Key of the tab this button represents. Used to detect selection.
+	 */
+	virtual void addButton(const std::string& label, const GUI::Widget::TabManager::TabKeyType& key);
+
+	/**
+	 * Remove a button from the list.
+	 *
+	 * @param key The key to identify the button.
+	 */
+	virtual void removeButton(const GUI::Widget::TabManager::TabKeyType& key);
+
+	/**
+	 * Recalculate button size after adding or removing.
+	 */
+	virtual void resizeButtons();
+
+public:
+	/**
+	 * Create a tab panel.
+	 *
+	 * @param x X coordinate of the tab panel.
+	 * @param y Y coordinate of the tab panel.
+	 * @param width Width of the tab panel.
+	 * @param height Height of the tab panel.
+	 */
+	TabPanel(float x, float y, float width, float height);
+
+	/**
+	 * Handle events.
+	 *
+	 * @param e The event.
+	 * @param window The window of the event.
+	 *
+	 * @return True, if this checkbox handled the event.
+	 */
+	virtual bool handleEvent(const sf::Event& e, const sf::RenderWindow& window);
+
+	/**
+	 * Draw the tab panel and objects in the selected tab.
+	 *
+	 * @param window The render window.
+	 */
+	virtual void draw(sf::RenderWindow& window);
+
+	/**
+	 * Update the widget state, if it needs to change without events.
+	 *
+	 * @param window The current window.
+	 */
+	virtual void updateState(sf::RenderWindow& window);
+
+	/**
+	 * Add a new tab.
+	 *
+	 * @param key Key for the new tab. If tab with same key already exists, it is replaced by the new one.
+	 * @param title Name for the tab.
+	 * @param container Container with objects for the tab. If one is not given it will be automatically created when needed.
+	 *
+	 * @return Returns shared_ptr to the container of the created tab.
+	 */
+	virtual TabContainerPtrType addTab(const TabKeyType& key, const std::string& title, TabContainerPtrType container = TabContainerPtrType());
+
+	/**
+	 * Remove a tab.
+	 *
+	 * @param key Key for the tab to remove.
+	 */
+	virtual void removeTab(const TabKeyType& key);
+};
+
+#endif
