7772727046023de74b226ac2d9e3744da8715a02
Lauri Kenttä <lauri.kentta@gmail.com>
2011-09-25 12:50:12 +0300

    connection/Base: Assign players to clients.

--- src/connection/Base.cpp
+++ src/connection/Base.cpp
@@ -29,8 +29,17 @@ void Connection::Base::initGame() {
 	map->load("maps/testmap");
 	game.reset(new Game::Game(map));
 
+	const Game::Game::PlayerContainerType& players(game->getPlayers());
+	Game::Game::PlayerContainerType::const_iterator p = players.begin();
+
 	for (ClientInfoContainerType::iterator i = clients.begin(); i != clients.end(); ++i) {
 		game->insertClient(i->second);
+		if (p != players.end()) {
+			i->second->players.insert(p++->first);
+		}
+	}
+	while (p != players.end()) {
+		clients.begin()->second->players.insert(p++->first);
 	}
 }
 
