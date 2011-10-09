48c4853a7fbc2ab7a6e78a46c61e560b4d70d007
Lauri Kenttä <lauri.kentta@gmail.com>
2011-10-09 19:31:54 +0300

    cli: Listen to both IPv4 and IPv6 and output some status messages.

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -38,7 +38,29 @@ try {
 	std::string title = ProgramInfo::name + " (version " + ProgramInfo::version + ", CLI)";
 	std::cout << title << std::endl;
 	boost::shared_ptr<Connection::Server> server(new Connection::Server());
-	server->addListener(boost::make_shared<Connection::TCPListener>(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 6667)));
+
+	int listeners = 0;
+	try {
+		std::cout << "Starting TCP listener on IPv6... ";
+		server->addListener(boost::make_shared<Connection::TCPListener>(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v6(), 6667)));
+		std::cout << "OK!\n";
+		++listeners;
+	} catch (std::runtime_error& e) {
+		std::cout << "Error! " << e.what() << "\n";
+	}
+	try {
+		std::cout << "Starting TCP listener on IPv4... ";
+		server->addListener(boost::make_shared<Connection::TCPListener>(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 6667)));
+		std::cout << "OK!\n";
+		++listeners;
+	} catch (std::runtime_error& e) {
+		std::cout << "Error! " << e.what() << "\n";
+	}
+	if (!listeners) {
+		std::cout << "No listeners, bailing out...\n";
+		return 1;
+	}
+	std::cout << "Listeners added, starting the main loop." << std::endl;
 	server->run();
 	return 0;
 } catch (std::exception& e) {
