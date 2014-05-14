b852cea4933dbc08c274ddc299cca766c8f2fb33
Lauri Kenttä <lauri.kentta@gmail.com>
2014-05-14 19:13:07 +0300

    Remove copyright notices from individual files.
    We already have LICENSE and AUTHORS, and Git knows who did what.

--- data/lua/Game.lua
+++ data/lua/Game.lua
@@ -1,21 +1,5 @@
--- DO NOT TOUCH! This is the Lua interface for the tech tree and scripts.
---
--- Copyright 2011 Lauri Kenttä
---
--- This file is part of PutkaRTS.
---
--- PutkaRTS is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- PutkaRTS is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with PutkaRTS.  If not, see <http://www.gnu.org/licenses/>.
+-- DO NOT TOUCH unless you're a developer!
+-- This is the Lua interface for the tech tree and scripts.
 
 --- Global methods and methods that interface with the whole game.
 Game = {
